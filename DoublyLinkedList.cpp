6b24dbfa9aece6b9264effbc5b30f92d6bf2d249
ferdaws21 <110644490+ferdaws21@users.noreply.github.com>
2024-10-13 22:51:21 -0700

    Update DoublyLinkedList.cpp

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -1,54 +1,58 @@
 #include "DoublyLinkedList.h"
 #include <iostream>
 
-Node::Node(Goat d) : data(d), next(nullptr), prev(nullptr) {}
-
 DoublyLinkedList::DoublyLinkedList() : head(nullptr), tail(nullptr) {}
 
-void DoublyLinkedList::push_front(Goat goat) {
-    Node* newNode = new Node(goat);
+void DoublyLinkedList::push_front(Goat g) {
+    Node* newNode = new Node(g);
     if (!head) {
         head = tail = newNode;
-    }
-    else {
+    } else {
         newNode->next = head;
         head->prev = newNode;
         head = newNode;
     }
 }
 
-void DoublyLinkedList::push_back(Goat goat) {
-    Node* newNode = new Node(goat);
+void DoublyLinkedList::push_back(Goat g) {
+    Node* newNode = new Node(g);
     if (!tail) {
         head = tail = newNode;
-    }
-    else {
+    } else {
         tail->next = newNode;
         newNode->prev = tail;
         tail = newNode;
     }
 }
 
-void DoublyLinkedList::printForward() const {
+void DoublyLinkedList::print_forward() const {
     if (!head) {
-        std::cout << "List is empty" << std::endl;
+        std::cout << "List is empty\n";
         return;
     }
     Node* current = head;
     while (current) {
-        current->data.print();
+        current->data.display();
         current = current->next;
     }
 }
 
-void DoublyLinkedList::printBackward() const {
+void DoublyLinkedList::print_backward() const {
     if (!tail) {
-        std::cout << "List is empty" << std::endl;
+        std::cout << "List is empty\n";
         return;
     }
     Node* current = tail;
     while (current) {
-        current->data.print();
+        current->data.display();
         current = current->prev;
+    }
+}
+
+DoublyLinkedList::~DoublyLinkedList() {
+    while (head) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
     }
 }
