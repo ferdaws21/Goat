fa1df6e90a0d9cbb32a37cd3a5db20f9027ab75d
ferdaws21 <110644490+ferdaws21@users.noreply.github.com>
2024-10-13 21:13:46 -0700

    GOAT.h

---
+++ GOAT.h
@@ -0,0 +1,32 @@
+#ifndef GOAT_H
+#define GOAT_H
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+class Goat {
+private:
+    int age;
+    std::string name;
+    std::string color;
+    
+    static const std::string names[15]; // Declare name array
+    static const std::string colors[15]; // Declare color array
+
+public:
+    // Default constructor
+    Goat();
+    // Parameter constructor
+    Goat(int a, std::string n, std::string c);
+
+    // Getters for printing
+    int getAge() const;
+    std::string getName() const;
+    std::string getColor() const;
+
+    // Print method for Goat
+    void print() const;
+};
+
+#endif // GOAT_H
