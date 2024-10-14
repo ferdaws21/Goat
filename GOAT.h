6f9811c6fd7239fb3b06eed8c67f14ef127f8a73
ferdaws21 <110644490+ferdaws21@users.noreply.github.com>
2024-10-13 22:53:19 -0700

    Update GOAT.h

--- GOAT.h
+++ GOAT.h
@@ -2,21 +2,24 @@
 #define GOAT_H
 
 #include <string>
+#include <cstdlib> // For rand() and srand()
 
-class GOAT {
+class Goat {
 private:
     int age;
     std::string name;
     std::string color;
-
-public:
     static const std::string names[15];
     static const std::string colors[15];
 
+public:
     Goat(); // Default constructor
-    Goat(int a, std::string n, std::string c); // Parameter constructor
+    Goat(int a, std::string n, std::string c); // Parameterized constructor
 
-    void display() const; // Method to display goat info
+    int getAge() const;
+    std::string getName() const;
+    std::string getColor() const;
+    void print() const;
 };
 
 #endif // GOAT_H
