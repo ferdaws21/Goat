85271a5f8a3602b23787ce3932cd54b9812f8af7
ferdaws21 <110644490+ferdaws21@users.noreply.github.com>
2024-10-13 22:25:15 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,24 @@
 #include <iostream>
-#include "GOAT.h"
+#include <cstdlib>
+#include <ctime>
 #include "DoublyLinkedList.h"
 
-using namespace std;
-
 int main() {
+    srand(time(0)); // Seed the random number generator
+
     DoublyLinkedList goatList;
+    int numGoats = rand() % 16 + 5; // Random number of goats between 5 and 20
 
-    
-    goatList.push_back(Goat("Pepper", "White", 15));
-    goatList.push_back(Goat("Teddy", "Purple", 20));
-    goatList.push_back(Goat("Socks", "Purple", 8));
-    goatList.push_back(Goat("Pepper", "Blue", 1));
-    goatList.push_back(Goat("Bella", "Pink", 10));
-    goatList.push_back(Goat("Zoe", "Black", 11));
-    goatList.push_back(Goat("Daisy", "Pink", 17));
-    goatList.push_back(Goat("Luna", "Yellow", 13));
-    goatList.push_back(Goat("Socks", "White", 14));
-    goatList.push_back(Goat("Ginger", "Brown", 13));
-    goatList.push_back(Goat("Socks", "Tan", 9));
-    goatList.push_back(Goat("Bella", "Blue", 1));
-    goatList.push_back(Goat("Ginger", "Golden", 6));
+    // Append random Goat objects to the list
+    for (int i = 0; i < numGoats; ++i) {
+        goatList.push_back(Goat()); // Using default constructor
+    }
 
-    cout << "Forward: " << endl;
+    // Print the list forward and backward
+    std::cout << "Forward: " << std::endl;
     goatList.printForward();
 
-    cout << "Backward: " << endl;
+    std::cout << "Backward: " << std::endl;
     goatList.printBackward();
 
     return 0;
