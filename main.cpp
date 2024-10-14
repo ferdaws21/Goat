f3c3b97fe65885a08566bcf18e8de9e10c33d7ea
ferdaws21 <110644490+ferdaws21@users.noreply.github.com>
2024-10-13 22:51:35 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
-#include "GOAT.h"             // Include your Goat class header
-#include "DoublyLinkedList.h"  // Include your DoublyLinkedList class header
-#include <cstdlib>             // For srand() and rand()
-#include <ctime>               // For time()
-
-using namespace std;
+#include <cstdlib>
+#include <ctime>
+#include "DoublyLinkedList.h"
 
 int main() {
     srand(time(0)); // Seed the random number generator
 
-    DoublyLinkedList goatList; // Create a DoublyLinkedList of Goat objects
-    int numGoats = rand() % 16 + 5; // Random number of goats between 5 and 20
+    DoublyLinkedList goatList;
 
-    for (int i = 0; i < numGoats; ++i) {
-        Goat goat; // Create a random Goat
-        goatList.push_back(goat); // Add the Goat to the list
+    // Create a random number of Goat objects (5 to 20)
+    int numberOfGoats = rand() % 16 + 5; // 5 to 20
+    for (int i = 0; i < numberOfGoats; ++i) {
+        Goat g; // Create a random Goat
+        goatList.push_back(g); // Add to the list
     }
 
-    cout << "Forward: " << endl;
-    goatList.printForward(); // Print the list forward
+    std::cout << "Forward traversal:\n";
+    goatList.print_forward();
 
-    cout << "Backward: " << endl;
-    goatList.printBackward(); // Print the list backward
+    std::cout << "\nBackward traversal:\n";
+    goatList.print_backward();
 
     return 0;
 }
