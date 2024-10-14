8c327c1b0ad09247d0656ad44ceb7966816110cf
ferdaws21 <110644490+ferdaws21@users.noreply.github.com>
2024-10-13 22:53:41 -0700

    Update DoublyLinkedList.h

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -9,19 +9,21 @@ private:
         Goat data;
         Node* next;
         Node* prev;
-        Node(Goat d) : data(d), next(nullptr), prev(nullptr) {}
+
+        Node(Goat d);
     };
-    
+
     Node* head;
     Node* tail;
 
 public:
-    DoublyLinkedList(); // Constructor
-    void push_front(Goat g); // Push front with Goat
-    void push_back(Goat g); // Push back with Goat
-    void print_forward() const; // Print forward
-    void print_backward() const; // Print backward
-    ~DoublyLinkedList(); // Destructor
+    DoublyLinkedList();
+    ~DoublyLinkedList();
+
+    void push_front(Goat goat);
+    void push_back(Goat goat);
+    void printForward() const;
+    void printBackward() const;
 };
 
 #endif // DOUBLYLINKEDLIST_H
