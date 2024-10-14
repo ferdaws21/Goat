e214bbf8cd1abe5b4ec1f5ee102dc6ad1e6612f2
ferdaws21 <110644490+ferdaws21@users.noreply.github.com>
2024-10-13 21:14:48 -0700

    Create DoublyLinkedList.h

---
+++ DoublyLinkedList.h
@@ -0,0 +1,28 @@
+#ifndef DOUBLY_LINKED_LIST_H
+#define DOUBLY_LINKED_LIST_H
+
+#include "FerdawsAlamyar_Goat.h"
+
+class Node {
+public:
+    Goat data; // Change from int to Goat
+    Node* next;
+    Node* prev;
+
+    Node(Goat d);
+};
+
+class DoublyLinkedList {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList();
+    void push_front(Goat goat);
+    void push_back(Goat goat);
+    void printForward() const;
+    void printBackward() const;
+};
+
+#endif // DOUBLY_LINKED_LIST_H
