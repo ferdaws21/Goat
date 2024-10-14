ae0643801c82fd62a8ad3d10a95cbabab76c0d8f
ferdaws21 <110644490+ferdaws21@users.noreply.github.com>
2024-10-13 22:24:53 -0700

    Update DoublyLinkedList.cpp

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -1,208 +1,52 @@
+#include "DoublyLinkedList.h"
 #include <iostream>
-using namespace std;
 
-const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
+Node::Node(Goat d) : data(d), next(nullptr), prev(nullptr) {}
 
-class DoublyLinkedList {
-private:
-    struct Node {
-        int data;
-        Node* prev;
-        Node* next;
-        Node(int val, Node* p = nullptr, Node* n = nullptr) : data(val), prev(p), next(n) {}
-    };
+DoublyLinkedList::DoublyLinkedList() : head(nullptr), tail(nullptr) {}
 
-    Node* head;
-    Node* tail;
-
-public:
-
-    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
-
-    void push_back(int value) {
-        Node* newNode = new Node(value);
-        if (!tail) {
-            head = tail = newNode; 
-        } else {
-            tail->next = newNode;
-            newNode->prev = tail;
-            tail = newNode;
-        }
+void DoublyLinkedList::push_front(Goat goat) {
+    Node* newNode = new Node(goat);
+    if (!head) {
+        head = tail = newNode;
+    } else {
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
     }
+}
 
-    void push_front(int value) {
-        Node* newNode = new Node(value);
-        if (!head) {
-            head = tail = newNode; 
-        } else {
-            newNode->next = head;
-            head->prev = newNode;
-            head = newNode;
-        }
+void DoublyLinkedList::push_back(Goat goat) {
+    Node* newNode = new Node(goat);
+    if (!tail) {
+        head = tail = newNode;
+    } else {
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
     }
+}
 
-    void insert_after(int value, int position) {
-        if (position < 0) {
-            cout << "Position must be >= 0." << endl;
-            return;
-        }
-        Node* newNode = new Node(value);
-        if (!head) {
-            head = tail = newNode;
-            return;
-        }
-        Node* temp = head;
-        for (int i = 0; i < position && temp; ++i) {
-            temp = temp->next;
-        }
-        if (!temp) {
-            cout << "Position exceeds list size. Node not inserted.\n";
-            delete newNode;
-            return;
-        }
-        newNode->next = temp->next;
-        newNode->prev = temp;
-        if (temp->next) {
-            temp->next->prev = newNode;
-        } else {
-            tail = newNode; 
-        }
-        temp->next = newNode;
+void DoublyLinkedList::printForward() const {
+    if (!head) {
+        std::cout << "List is empty" << std::endl;
+        return;
     }
-
-    void delete_val(int value) {
-        if (!head) return; 
-        Node* temp = head;
-        while (temp && temp->data != value) {
-            temp = temp->next;
-        }
-        if (!temp) return; 
-        if (temp->prev) {
-            temp->prev->next = temp->next;
-        } else {
-            head = temp->next; 
-        }
-        if (temp->next) {
-            temp->next->prev = temp->prev;
-        } else {
-            tail = temp->prev; 
-        }
-        delete temp;
+    Node* current = head;
+    while (current) {
+        current->data.print();
+        current = current->next;
     }
+}
 
-    void delete_pos(int position) {
-        if (position < 0 || !head) return; 
-        Node* temp = head;
-        for (int i = 0; i < position && temp; ++i) {
-            temp = temp->next;
-        }
-        if (!temp) return; 
-
-     
-        if (temp->prev) {
-            temp->prev->next = temp->next;
-        } else {
-            head = temp->next; 
-        }
-        if (temp->next) {
-            temp->next->prev = temp->prev;
-        } else {
-            tail = temp->prev; 
-        }
-        delete temp;
+void DoublyLinkedList::printBackward() const {
+    if (!tail) {
+        std::cout << "List is empty" << std::endl;
+        return;
     }
-
-    void pop_front() {
-        if (!head) return; 
-        Node* temp = head;
-        head = head->next;
-        if (head) head->prev = nullptr;
-        else tail = nullptr; 
-        delete temp;
+    Node* current = tail;
+    while (current) {
+        current->data.print();
+        current = current->prev;
     }
-
-    void pop_back() {
-        if (!tail) return; 
-        Node* temp = tail;
-        tail = tail->prev;
-        if (tail) tail->next = nullptr;
-        else head = nullptr; 
-        delete temp;
-    }
-
-    void print() {
-        Node* current = head;
-        if (!current) {
-            cout << "List is empty" << endl;
-            return;
-        }
-        while (current) {
-            cout << current->data << " ";
-            current = current->next;
-        }
-        cout << endl;
-    }
-
-    void print_reverse() {
-        Node* current = tail;
-        if (!current) {
-            cout << "List is empty" << endl;
-            return;
-        }
-        while (current) {
-            cout << current->data << " ";
-            current = current->prev;
-        }
-        cout << endl;
-    }
-
-    ~DoublyLinkedList() {
-        while (head) {
-            Node* temp = head;
-            head = head->next;
-            delete temp;
-        }
-    }
-};
-
-
-int main() {
-    DoublyLinkedList list;
-    int size = rand() % (MAX_LS - MIN_LS + 1) + MIN_LS;
-
-  
-    for (int i = 0; i < size; ++i)
-        list.push_back(rand() % (MAX_NR - MIN_NR + 1) + MIN_NR);
-
-    cout << "List forward: ";
-    list.print();
-    
-    cout << "List backward: ";
-    list.print_reverse();
-
- 
-    list.pop_front();
-    cout << "After pop_front: ";
-    list.print();
-    
-    list.pop_back();
-    cout << "After pop_back: ";
-    list.print();
-
-  
-    list.delete_pos(2); 
-    cout << "After delete_pos(2): ";
-    list.print();
-
-   
-    list.delete_val(15); 
-    cout << "After delete_val(15): ";
-    list.print();
-
-    cout << "Deleting list, then trying to print.\n";
-    list.~DoublyLinkedList();
-    cout << "List forward: ";
-    list.print();
-    
-    return 0;
 }
